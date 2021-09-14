823f230068e608442e0f00588a2dbfa7659cd420
laraxpress <mazed@isrt.ac.bd>
2021-09-14 20:32:22 +0600

    OOP : Object Iteration

---
+++ OOP ~ Object Iteration/object_iteration.php
@@ -0,0 +1,44 @@
+<?php
+	
+	class Student{
+		public $name;
+		public $course;
+		public $session;
+		public $branch;
+
+		private $income = 12500;		
+
+		public function __construct($name,$course,$session,$branch)
+		{
+		    $this->name   = $name;
+		    $this->course = $course;
+		    $this->session= $session;
+		    $this->branch = $branch;
+		}
+
+		public function getDetails()
+		{
+		    return $this->income;
+		}
+
+		public function getSalary()
+		{
+		    foreach ($this as $key => $value) {
+		        echo '<pre>';
+			    echo "$key => $value";
+			    echo '</pre>';
+		    }
+
+		}
+	}
+
+	$std = new Student('Mazedur','MBA','2002-03','Gulshan');
+	echo '<b>Object iteration only public access</b>';
+	foreach ($std as $key => $value) {
+	    echo '<pre>';
+	    echo "$key => $value";
+	    echo '</pre>';
+	}
+	echo '<hr/>';
+	echo '<b>Object iteration with all activities i.e. private as well as protected members</b>';
+	$std->getSalary();
